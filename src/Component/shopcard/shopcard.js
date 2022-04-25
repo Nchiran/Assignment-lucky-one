b705cadcfb50d66b07d6c326fc16d533e348b1d0
NidraChiran <nclarachiran@gmail.com>
2022-04-25 21:08:04 +0600

    my git commit

---
+++ src/Component/shopcard/shopcard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ShopCard.css";
+
+const Shopingcard = () => {
+  return (
+    <div>
+      <div></div>
+      <button className="btn">
+        <p className="btn-text">Add to Cart</p>
+      </button>
+      <button className="btn">
+        <p className="btn-text">Add to Cart</p>
+      </button>
+    </div>
+  );
+};
+
+export default Shopingcard;
